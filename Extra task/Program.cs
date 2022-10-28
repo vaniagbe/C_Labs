613955be595128ce65d9c963180d57b1850e2c80
vania <89963197+vaniagbe@users.noreply.github.com>
2022-10-28 15:56:38 +0300

    Add files via upload

---
+++ Extra task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace HoMEwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            double a, b, c, perimeter;
+            int stepen = 2;
+            Console.WriteLine("Enter first side :");
+            str = Console.ReadLine();
+            a = Convert.ToDouble(str);
+            Console.WriteLine("Enter second side :");
+            str = Console.ReadLine();
+            b = Convert.ToDouble(str);
+            Console.WriteLine("Enter hypotenuse side :");
+            str = Console.ReadLine();
+            c = Convert.ToDouble(str);
+            double g = Math.Pow(c, stepen);
+            double g1 = Math.Pow(a, stepen);
+            double g2 = Math.Pow(b, stepen);
+            if (g == g1 + g2)
+            {
+                perimeter = 0.5 * (a * b);
+                Console.WriteLine(perimeter);
+            }
+            else
+            {
+                Console.WriteLine("It's not a rectangular triangle");
+            }
+        }
+    }
+}
