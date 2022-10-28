4aca54db43dcdd1b946532dfcf2498e131816eaf
vania <89963197+vaniagbe@users.noreply.github.com>
2022-10-28 18:51:57 +0300

    Add files via upload

---
+++ Lab4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of array :");
+            int N = int.Parse(Console.ReadLine());
+            int[] array = new int[N];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(array.Max());
+            Console.WriteLine(array.Min());
+        }
+    }
+}
+    
+
