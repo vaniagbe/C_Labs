3719915f6a0fd4bbc335cf5b7e643ba3ee69dfab
vania <89963197+vaniagbe@users.noreply.github.com>
2022-10-28 15:55:26 +0300

    Add files via upload

---
+++ Lab2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            if (k < 0)
+            {
+                Console.WriteLine("Mistake");
+            }
+            else
+            {
+                double expression = (Math.Pow(-1, k) + k + 8) / (2 * (Math.Pow(k, 2)) + k + 7);
+                Console.WriteLine(expression);
+            }
+        }
+    }
+}
